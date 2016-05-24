a852a0665bc646c2b4cc7ed5d39d84f28783bf6a
chenl_cl <347979248@qq.com>
2016-05-24 19:46:19 +0800

    增加了代码

---
+++ xmpp1/xmpp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xmpp1
+//
+//  Created by mac on 16/5/24.
+//  Copyright © 2016年 sunsmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xmpp1/xmpp1/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  xmpp1
+//
+//  Created by mac on 16/5/24.
+//  Copyright © 2016年 sunsmart. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self configUI];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+#pragma mark -
+#pragma mark 初始化UI
+-(void)configUI{
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+#pragma mark -
+#pragma mark 加载数据
+
+#pragma mark -
+#pragma mark 事件
+
+#pragma mark -
+#pragma mark 数据请求
+
+#pragma mark -
+#pragma mark 代理
+
+#pragma mark -
+#pragma mark 业务逻辑
+
+#pragma mark -
+#pragma mark 通知注册和销毁
+
+@end
